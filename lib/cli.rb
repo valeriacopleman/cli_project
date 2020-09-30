c735d8142560c75220900f91a13ce61aa881957c
valeriacopleman <valeriacopleman@gmail.com>
2020-09-30 10:30:58 -0700

    add starter method

---
+++ lib/cli.rb
@@ -0,0 +1,13 @@
+class CLI
+
+    def starter
+        puts ""
+        puts "Welcome to Breaking Bad Lines!"
+        puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+        puts ""
+        puts "Enter the name of your favorite Breaking Bad character from the list: "
+
+    end
+
+end
+
