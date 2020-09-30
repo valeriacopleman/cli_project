86131f90f5c77cef1c5b7ede30019e6a7d8c8618
valeriacopleman <valeriacopleman@gmail.com>
2020-09-30 11:52:21 -0700

    add puts lines

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,10 +2,15 @@ class CLI
 
     def starter
         puts ""
-        puts "Welcome to Breaking Bad Lines!"
+        puts "Welcome to The Breaking Bad Lines!"
         puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
         puts ""
-        puts "Enter the name of your favorite Breaking Bad character from the list: "
+        puts "AMC's Breaking Bad is among the top shows ever created."
+        puts "Would you like to find some of the famous quotes from the characters?"
+        puts "Enter Yes to find some quotes or Exit to leave the program: "
+        puts ""
+        puts "Enter the name of your favorite Breaking Bad character from the list to get some of your favorite lines: "
+        puts ""
 
     end
 
