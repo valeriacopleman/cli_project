1f25bdb4a9a6aa322b55f25fe1fa41a3c131daf7
valeriacopleman <valeriacopleman@gmail.com>
2020-10-01 13:31:48 -0700

    getting quote, but not printing

--- lib/api.rb
+++ lib/api.rb
@@ -1,13 +1,10 @@
 class API
 
     def self.get_list
-        #binding.pry
         url = "https://www.breakingbadapi.com/api/characters?category=Breaking+Bad"
         uri = URI(url)
         response = Net::HTTP.get(uri)
-        #binding.pry
         characters = JSON.parse(response)
-        # use data from list to make all character objects
         characters.each do |char|
           Character.new(name: char["name"]) 
         end
@@ -15,14 +12,15 @@ class API
 
 
     def self.get_quote(character)
-        #binding.pry
         url = "https://www.breakingbadapi.com/api/quote/random?author=#{character.name.gsub(' ','+')}"
         uri = URI(url)
         response = Net::HTTP.get(uri)
-        #binding.pry
         quote = JSON.parse(response)
-        #quote
+        #binding.pry
+        quote.each do |lines|
+            puts lines["quote"]
+        end
     end
 
-
 end
+
--- lib/cli.rb
+++ lib/cli.rb
@@ -4,7 +4,6 @@ class CLI
 
         API.get_list
 
-        puts ""
         puts "      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
         puts "      Welcome to The Breaking Bad Lines!"
         puts "      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
@@ -12,29 +11,24 @@ class CLI
         print_list
         puts ""
         puts "Enter the number of your favorite Breaking Bad character to get some lines or 'Exit' to exit the program: "
-        puts ""
         input = gets.strip.downcase 
-        #binding.pry
         while input != 'exit' do
             if input.to_i > 0 && input.to_i <= print_list.count
-                puts ""
-                character = Character.all.at(input.to_i - 1) 
-                quote = API.get_quote(character)
-                puts quote
-               #get number and matching name to get 
+                puts "" 
+                #API.get_quote(Character.all.at(input.to_i - 1)) 
             else
                 puts "~~Oops! Invalid response. Please try again.~~"
             end
-            puts ""
-            puts "Enter the number of your favorite Breaking Bad character to get some lines or 'Exit' to exit the program: "
-            puts ""
-            input = gets.strip.downcase
+                puts ""
+                puts "Enter the number of your favorite Breaking Bad character to get some lines or 'Exit' to exit the program: "
+                puts ""
+                input = gets.strip.downcase
         end
-        puts ""
-        puts "      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-        puts "      Thank you for using The Breaking Bad Lines!"
-        puts "      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-        puts ""
+            puts ""
+            puts "      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+            puts "      Thank you for using The Breaking Bad Lines!"
+            puts "      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+            puts ""
     end
 
     def print_list
@@ -42,21 +36,6 @@ class CLI
             puts "#{i}. #{c.name}"
         end
     end
-
-    #def print_quote(quote)
-
-    #end
-
-        # get that character from character.all 
-        #call API.get_quote(character) from the line above
-        #print quote
-        #q = API.get_quote(Character.new(name: "Jesse Pinkman"))
-        #binding.pry
-        #puts q 
-
-
-
-    
 
 end
 
